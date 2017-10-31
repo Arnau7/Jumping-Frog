5069a10ce0bd1de652cf4b5cc97721e2b8c709df
Arnau Ruiz <arnauruiz1b@gmail.com>
2017-10-31 13:03:23 +0100

    Froggy

---
+++ Roll-a-ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+    private Vector3 offsetR;
+    Vector3 leftTurn;
+    Vector3 rightTurn;
+
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - player.transform.position;
+       
+        leftTurn = new Vector3(0, -0.5f, 0);
+        rightTurn = new Vector3(0, +0.5f, 0);
+    }
+	
+	// Runs every frame, after all items have been processed
+	void LateUpdate ()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(leftTurn,Space.World);
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(rightTurn, Space.World);
+        }
+        transform.position = player.transform.position + offset;
+      
+
+    }
+}
---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    //public bool isMoving;
+    Animator animation;
+    Vector3 leftTurn;
+    Vector3 rightTurn;
+    public Rigidbody rb;
+    public GameObject resetButton;
+
+    private Vector3 inicialPosition;
+    private Quaternion inicialRotation;
+
+    void Start()
+    {
+        inicialPosition = transform.position;
+        inicialRotation = transform.rotation;
+
+        animation = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+
+        leftTurn = new Vector3(0, -0.5f, 0);
+        rightTurn = new Vector3(0, +0.5f, 0);
+    }
+    //Before rendering a frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.transform.position += Vector3.forward * speed * Time.deltaTime;
+            //isMoving = true;
+            animation.SetBool("Moving", true);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.transform.position += Vector3.back * speed * Time.deltaTime;
+            //isMoving = true;
+            animation.SetBool("Moving", true);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.transform.Rotate(leftTurn,Space.World);
+            animation.SetBool("Moving", false);
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.transform.Rotate(rightTurn,Space.World);
+            animation.SetBool("Moving", false);
+        }
+        else
+        {
+            animation.SetBool("Moving", false);
+           // isMoving = false;
+        }
+ 
+    }
+    public void ResetPosition()
+    {
+        transform.position = inicialPosition;
+        transform.rotation = inicialRotation;
+    }   
+    //Before performing physics calculations
+    //void FixedUpdate()
+    //{
+    //    float moveHorizontal = Input.GetAxis("Horizontal");
+    //    float moveVertical = Input.GetAxis("Vertical");
+
+    //    Vector3 movement = new Vector3(moveHorizontal, .0f, moveVertical);
+
+    //    rb.AddForce(movement * speed);
+
+    //}
+
+}
